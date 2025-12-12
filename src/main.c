69c0039391c35f42ec3be9da99e5a234069a5672
8omp <mendechongtai@gmail.com>
2025-12-12 11:04:01 +0900

    edit main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,23 @@
 #include "scanner.h"
+#include "utils.h"
 
 int main(int argc, char *argv[]) {
     char ip_address[16];
+    char input[256];
 
     if (argc > 1) {
-        strncpy(ip_address, argv[1], 15);
-        ip_address[15] = '\0';
+        strncpy(input, argv[1], sizeof(input) - 1);
+        input[sizeof(input) - 1] = '\0';
     } else {
         printf("Usage: %s <IP_ADDRESS>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
+    if(resolve_hostname(input, ip_address, sizeof(ip_address)) != 0){
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Target: %s (%s)\n", input, ip_address);
     run_threaded_scan(ip_address);
 
     return 0;
