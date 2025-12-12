6a5822f330dd1a11f4d871d08b37385678f5d5a6
8omp <mendechongtai@gmail.com>
2025-12-12 11:03:53 +0900

    edit utils.h

--- include/utils.h
+++ include/utils.h
@@ -2,9 +2,10 @@
 #define UTILS_H
 
 #include <stddef.h>
+#include <stdbool.h>
 
 // DNS関連のユーティリティ関数群
 int resolve_hostname(const char *hostname, char *ip_out, size_t ip_out_size);
-int is_valid_ipv4(const char *str);
+bool is_valid_ipv4(const char *str);
 
 #endif // UTILS_H
